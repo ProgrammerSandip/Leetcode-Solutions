2ec6661712f42b88eaa8613dc88a06b856e2bec3
Programmer Sunny <159574454+ProgrammerSandip@users.noreply.github.com>
2026-07-28 22:41:46 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.86%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,5 +1,24 @@
 class Solution {
     public int fib(int n) {
+        // For Loop method
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int first = 0;
+        int second = 1;
+        int current = 0;
+
+        for (int i = 2; i <= n; i++) {
+            current = first + second;
+            first = second;
+            second = current;
+        }
+
+        return current;
+    }
+
+    /* Recursion method
+    public int fib(int n) {
         return f(n);
     }
     int f(int n)
@@ -10,4 +29,5 @@ class Solution {
         }
         return f(n-1) + f(n-2);
     }
+    */
 }
