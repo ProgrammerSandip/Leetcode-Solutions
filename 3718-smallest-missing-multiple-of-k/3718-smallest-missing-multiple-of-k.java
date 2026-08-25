64ddb0d32133199443b1ec56f883c112fa310a05
Programmer Sunny <159574454+ProgrammerSandip@users.noreply.github.com>
2026-08-25 20:38:49 +0530

    Time: 6 ms (10.64%), Space: 45.3 MB (52.43%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -13,3 +13,28 @@ class Solution {
         return multiple;
     }
 }
+
+/*
+Approach 2, using HashSet
+
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        // Store all numbers in the Set
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        // Start checking multiples of k
+        int multiple = k;
+
+        while (set.contains(multiple)) {
+            multiple = multiple + k;
+        }
+
+        return multiple;
+    }
+}
+*/
